69e58de33adae2c53d6e391eec16b46628c35ab9
angelosorbo <asorbo@yahoo.com>
2023-07-28 15:45:23 +0100

    Prima revisione.

---
+++ src/Conto_Corrente/Banca.java
@@ -0,0 +1,11 @@
+package Conto_Corrente;
+
+public class Banca {
+    public static void main(String args[]) {
+        Conto_Corrente mioCC = new Conto_Corrente("Rossi", "Mario");
+        new Versamento(mioCC, 1020, "Stipendio");
+        new Prelievo(mioCC, 250, "Bancomat");
+        new Versamento(mioCC, 120, "Contanti");
+        new Prelievo(mioCC, 87, "Bolletta Gas");
+    }
+}
---
+++ src/Conto_Corrente/Conto_Corrente.java
@@ -0,0 +1,65 @@
+package Conto_Corrente;
+
+class Conto_Corrente {
+    String cognome;
+    String nome;
+    int saldo;
+    public Conto_Corrente(String cog, String nom) {
+        cognome = cog;
+        nome = nom;
+        saldo = 0;
+    }
+
+    synchronized void aggiungi(int cifra) {
+       saldo += cifra;
+    }
+    synchronized void sottrai(int cifra) {
+       saldo -= cifra;
+    }
+    synchronized void stampa_saldo() {
+       System.out.println("Saldo del sig. " + cognome + " " + nome + " = " + saldo);
+    }
+}
+
+class Prelievo implements Runnable {
+    Conto_Corrente conto_corrente;
+    int cifra;
+    String motivo;
+        Prelievo(Conto_Corrente un_cc, int c, String m) {
+        conto_corrente = un_cc;
+        cifra = c;
+        motivo = m;
+        new Thread(this).start();
+    }
+    public void run() {
+        try {
+            Thread.sleep((int)(Math.random()*10000));    // attesa fittizia
+        } catch (Exception e) {};
+        conto_corrente.sottrai(cifra);
+        System.out.println("Prelievo del sig. " + conto_corrente.cognome +
+                ": Euro " + cifra + " Causale: " + motivo);
+        conto_corrente.stampa_saldo();
+    }
+}
+
+class Versamento implements Runnable {
+    Conto_Corrente conto_corrente;
+    int cifra;
+    String motivo;
+        Versamento(Conto_Corrente un_cc, int c, String m) {
+        conto_corrente = un_cc;
+        cifra = c;
+        motivo = m;
+        new Thread(this).start();
+    }
+    public void run() {
+         try {
+            Thread.sleep((int)(Math.random()*10000));    // attesa fittizia
+        } catch (Exception e) {};
+        conto_corrente.aggiungi(cifra);
+        System.out.println("Versamento del sig. " + conto_corrente.cognome +
+                ": Euro " + cifra + " Causale: " + motivo);
+        conto_corrente.stampa_saldo();
+    }
+}
+
